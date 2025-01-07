9ce27b013aa80714fb7f4118ef57c51d3d891f3f
Rafsan <csrafsan700@gmail.com>
2025-01-07 08:06:47 +0600

    update-user-info

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -277,3 +277,38 @@ export const socialAuth = CatchAsyncError(
     }
   }
 );
+
+// update user info
+interface IUpdateUserInfo{
+  name?: string;
+  email?: string;
+}
+
+export const updateUserInfo = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { name, email } = req.body as IUpdateUserInfo;
+      const userId = req.user?._id;
+      const user = await userModel.findById(userId);
+
+      if (email && user) {
+        const isEmailExist = await userModel.findOne({ email });
+        if (isEmailExist) {
+          return next(new ErrorHandler("Email already exist", 400));
+        }
+        user.email = email;
+      }
+      if (name && user) {
+        user.name = name;
+      }
+      await user?.save();
+      await redis.set(userId as string, JSON.stringify(user));
+      res.status(201).json({
+        success: true,
+        user,
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,17 +1,24 @@
-import express from 'express';
-import { activateUser, getUserInfo, loginUser, logoutUser, registrationUser, socialAuth, updateAccessToken } from './../controllers/user.controller';
-import { authorizeRoles, isAuthenticated } from '../middleware/auth';
+import express from "express";
+import {
+  activateUser,
+  getUserInfo,
+  loginUser,
+  logoutUser,
+  registrationUser,
+  socialAuth,
+  updateAccessToken,
+  updateUserInfo,
+} from "./../controllers/user.controller";
+import { authorizeRoles, isAuthenticated } from "../middleware/auth";
 const userRouter = express.Router();
 
-userRouter.post('/registration', registrationUser);
-userRouter.post('/activate-user', activateUser);
-userRouter.post('/login', loginUser);
-userRouter.get('/logout',isAuthenticated, logoutUser);
-userRouter.get('/refresh', updateAccessToken);
-userRouter.get('/me', isAuthenticated, getUserInfo);
-userRouter.post('/social-auth', socialAuth );
+userRouter.post("/registration", registrationUser);
+userRouter.post("/activate-user", activateUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/logout", isAuthenticated, logoutUser);
+userRouter.get("/refresh", updateAccessToken);
+userRouter.get("/me", isAuthenticated, getUserInfo);
+userRouter.post("/social-auth", socialAuth);
+userRouter.put("/update-user-info", isAuthenticated, updateUserInfo);
 
 export default userRouter;
-
-
-
